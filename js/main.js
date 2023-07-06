ee7e0e745fa81664196ca481f450b0dbb71c733d
christianjjc <christianjjc@gmail.com>
2023-07-05 19:40:10 -0500

    se borra el codigo comentado

--- js/main.js
+++ js/main.js
@@ -6,88 +6,12 @@ let i = 0;
 let contador_mensajes = 0;
 let contadorseundos = 0;
 
-// function escribirTexto(texto, idEtiqueta, mls) {
-//   const textoElemento = document.getElementById(idEtiqueta);
-//   const cantCar = texto.length;
-//   const intervaloCaracter = setInterval(() => {
-//     const caracter = texto.slice(i, i + 1);
-//     textoescribir += caracter;
-//     let nuevomensaje = textoescribir.replace("$", `<span class="dollar">$</span>`);
-//     textoElemento.innerHTML = nuevomensaje;
-//     i++;
-//     if (i == cantCar) {
-//       clearInterval(intervaloCaracter);
-//       nuevomensaje = nuevomensaje.replace("Christian Jiménez C.", `<span class="dollar">Christian Jiménez C.</span>`);
-//       nuevomensaje = nuevomensaje.replace("/**", `<span class="comented">/**`, "*/", `*/</span>`);
-//       nuevomensaje = nuevomensaje.replace("Full Stack Javascript Developer", `<span class="dollar">Full Stack Javascript Developer</span>`);
-//       textoElemento.innerHTML = nuevomensaje;
-//       i = 0;
-//       textoescribir = "";
-//     }
-//   }, mls);
-// }
-
-// function frasesEscritas(cadaXsegundos, texto1, idEtiquetaNueva, mls, cursor = false, idEtiquetaAnt = "") {
-//   const contenedor = document.getElementById("contenedor-mensajes");
-//   code = document.createElement("code");
-//   code.setAttribute("id", idEtiquetaNueva);
-//   contador_mensajes == 0 && code.classList.add("typed");
-//   contenedor.appendChild(code);
-//   setTimeout(() => {
-//     if (cursor) {
-//       if (contador_mensajes > 0) {
-//         const txtsale = document.getElementById(idEtiquetaAnt);
-//         txtsale.classList.remove("typed");
-//         const txtentra = document.getElementById(idEtiquetaNueva);
-//         txtentra.classList.add("typed");
-//       }
-//     }
-//     escribirTexto(texto1, idEtiquetaNueva, mls);
-//   }, cadaXsegundos * 1000);
-//   contador_mensajes++;
-// }
-
-// function cargaMensajes() {
-//   const texto1 = `$ ¡Hola! Bienvenido a mi Web Site. `;
-//   const texto2 = `$ Mi nombre es Christian Jiménez C.`;
-//   const texto3 = `$ /** --------------------- */`;
-//   const texto4 = `$ Me desempeño como Full Stack Javascript Developer.`;
-//   const texto5 = `$ A continuación, te invito a conocer más sobre mi y mis proyectos.`;
-//   let summartiempo = 0;
-//   frasesEscritas(summartiempo, texto1, "typed-1", 40);
-//   frasesEscritas((summartiempo += 2.5), texto2, "typed-2", 40, true, "typed-1");
-//   frasesEscritas((summartiempo += 2.5), texto3, "typed-3", 40, true, "typed-2");
-//   frasesEscritas((summartiempo += 2.5), texto4, "typed-4", 40, true, "typed-3");
-//   frasesEscritas((summartiempo += 3), texto5, "typed-5", 40, true, "typed-4");
-//   setTimeout(() => {
-//     agrandaPizarra();
-//   }, (summartiempo + 4) * 1000);
-// }
-
 function iniciaPagina() {
-  // const divBotonera = document.querySelector("#botonera");
-  // const divPizarra = document.getElementById("pizarra-codigo");
   const divPrecarga = document.getElementById("precarga");
-
   setTimeout(() => {
     divPrecarga.classList.add("oculto");
     showProfileCard();
   }, 5500);
-
-  // setTimeout(() => {
-  //   divPizarra.classList.add("pizarra-animate--height");
-  //   divPizarra.classList.add("pizarra-codigo--estilos");
-  //   divBotonera.classList.toggle("oculto");
-  // }, 6000);
-
-  // setTimeout(() => {
-  //   cargaMensajes();
-  // }, 7000);
-
-  // setTimeout(() => {
-  //   divPizarra.classList.remove("pizarra-animate--height");
-  //   divPizarra.classList.add("height-auto");
-  // }, 8000);
 }
 
 iniciaPagina();
